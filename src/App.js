285d6ff666597e0829f237ed4781563ff90e7d9f
ThiagoDiniz-creator <thiago.diniz.20.03.2002@gmail.com>
2021-03-04 16:20:21 -0300

    the upload of the first files of the project, just to start the repository

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//importing React
+import { React } from 'react';
+
+//importing styles
+import './App.css';
+
+//importing components
+import { HomePage } from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
